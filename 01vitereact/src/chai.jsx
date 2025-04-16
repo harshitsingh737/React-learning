cb2416045359df7c8cd0e48408b948f9c0fa7837
Harshit Singh <harshitsinghprof@gmail.com>
2025-04-17 01:17:36 +0530

    custom react

--- 01vitereact/src/chai.jsx
+++ 01vitereact/src/chai.jsx
@@ -1,6 +1,12 @@
 function Chai(){
+    const username = 'GL BAJAJ'
     return (
-        <h1>Chai aur code</h1>
+       <>
+        {/* Important === {username}  this is called evaluated expression. we do not write javaScript in it . We only write the final outcome of JS that has already been evaluated*/}
+       <h1>Chai or code {username}</h1>
+       <p>hello world</p>
+       <h2>Harshit</h2>
+       </>
     )
 }
 
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,10 +1,47 @@
+import React from 'react'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
+function MyApp(){
+    return (
+      <div>
+        <h1>My custom App</h1>
+      </div>
+    )
+}
+
+// const reactElement = {
+//   type: 'a',
+//   props: {
+//       href: 'https://www.google.com/',
+//       target: '_blank'
+//   },
+//   children:'Click me to visit google'
+// }
+
+const anotherElement = (
+  <a href="https://www.google.com/" target='_blank'>Visit Google</a>
+)
+
+const username = 'chai aur code'
+
+const reactElement = React.createElement(
+  // 1st parameter is tag
+  'a',
+  // 2nd parameter is an object with all attributes
+  
+      {href :'https://www.google.com/', target:'_black'},
+
+      // 3rd parameter is direct text
+      'Click to visit google',
+
+      // after that we can injest all the variables
+      username
+  
+)
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  reactElement
+  // <App />
 )
